534613864b1fb580d2c3b45d230dfc834130915b
Bobbyztz <42489536+Bobbyztz@users.noreply.github.com>
2023-01-14 23:14:49 -0600

    test pull

--- src/main/java/test/copilot0/Main.java
+++ src/main/java/test/copilot0/Main.java
@@ -13,6 +13,7 @@ public class Main {
         System.out.println("hi 2_1");
         System.out.println("hi 2_2");
         System.out.println("hi 3_0");
+        System.out.println("test pull");
     }
 
 
