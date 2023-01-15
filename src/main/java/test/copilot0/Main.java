8ec6b0bd8846fe54d67bf1d5da57713ee2c67b92
Zhang <legendztz@outlook.com>
2023-01-14 22:13:24 -0600

    hi 1_2
    hi 1_1
    
    hi 1_0

--- src/main/java/test/copilot0/Main.java
+++ src/main/java/test/copilot0/Main.java
@@ -5,6 +5,9 @@ public class Main {
         System.out.println("hi 0");
         System.out.println("hi 0_1");
         System.out.println("hi 0_2");
+        System.out.println("hi 1_0");
+        System.out.println("hi 1_1");
+        System.out.println("hi 1_2");
     }
 
 
