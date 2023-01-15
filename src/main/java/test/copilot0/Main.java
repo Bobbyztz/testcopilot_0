4a652a8f87fb207dcf794ad8d423980f9dc19495
Zhang <legendztz@outlook.com>
2023-01-14 21:54:55 -0600

    hi 0_2
    hi 0_1
    
    hi_0

--- src/main/java/test/copilot0/Main.java
+++ src/main/java/test/copilot0/Main.java
@@ -1,5 +1,11 @@
-package test.copilot0;public class Main {
+package test.copilot0;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("hi 0");
+        System.out.println("hi 0_1");
+        System.out.println("hi 0_2");
     }
+
+
 }
