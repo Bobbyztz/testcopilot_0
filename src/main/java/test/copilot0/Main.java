823da86ebaea15838ad8d28d9772a80b424e2642
Zhang <legendztz@outlook.com>
2023-01-18 21:30:03 -0600

    issue 1
    br_1_0
    
    br_1_1
    
    br_1_2

--- src/main/java/test/copilot0/Main.java
+++ src/main/java/test/copilot0/Main.java
@@ -2,19 +2,9 @@ package test.copilot0;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi 0");
-        System.out.println("hi 0_1");
-        System.out.println("hi 0_2");
-        System.out.println("hi 0_3");
         System.out.println("hi 1_0");
         System.out.println("hi 1_1");
         System.out.println("hi 1_2");
-        System.out.println("hi 2_0");
-        System.out.println("hi 2_1");
-        System.out.println("hi 2_2");
-        System.out.println("hi 3_0");
-        System.out.println("test pull");
-        System.out.println("test pull");
     }
 
 
